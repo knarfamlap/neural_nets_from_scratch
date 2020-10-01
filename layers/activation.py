d63b2b595256fb5cc85f5cfdea5ea51fcba26fba
Frank Palma <frankpalma12@gmail.com>
2020-10-01 18:42:29 -0400

    adds layers, activations, and loss

---
+++ layers/activation.py
@@ -0,0 +1,26 @@
+from layer import Layer
+import numpy as np
+
+class Activation(Layer):
+    def __init__(self, activation="relu"):
+        if activation == "relu":
+            self.activation = self.tanh
+            self.activation_prime = self.tanh_prime
+        else:
+            raise ValueError("Only supported activations are: relu")
+
+    def forward_propagation(self, input_data):
+        self.input = input_data
+        self.output = self.activation(self.input)
+
+        return self.output
+
+    def backward_propagation(self, output_error, learning_rate):
+        return self.activation_prime(self.input) * output_error
+
+
+    def tanh(self, x):
+        return np.tanh(x)
+
+    def tanh_prime(self, x):
+        return 1 - np.tanh(x) ** 2
