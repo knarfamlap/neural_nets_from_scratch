73b5a466f260816ba5e7ae171db1cb452be24e48
knarfamlap <frankpalma12@gmail.com>
2020-12-06 22:39:04 -0500

    adds lstm file

---
+++ lstm.py
@@ -0,0 +1,13 @@
+import numpy as np
+from layer import Layer
+
+
+class LSTM(Layer):
+    def __init(self):
+        pass
+
+    def forward(self):
+        pass
+
+    def backward(self):
+        pass
