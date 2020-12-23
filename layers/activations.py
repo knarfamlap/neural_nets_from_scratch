927c3b6afb40508ea8d9ff2d860ccc97b5dd9d1c
knarfamlap <frankpalma12@gmail.com>
2020-12-22 22:36:50 -0500

    update lstm

--- layers/activations.py
+++ layers/activations.py
@@ -3,3 +3,15 @@ import numpy as np
 
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
+
+
+def d_sigmoid(x):
+    return x * (1 - x)
+
+
+def tanh(x):
+    return np.tanh(x)
+
+
+def d_tanh(x):
+    return 1 - x * x
--- layers/lstm.py
+++ layers/lstm.py
@@ -7,26 +7,26 @@ class LSTM(Layer):
     def __init(self, input_size, output_size, hidden_size=64):
 
         # init weights
-        self.Wf = randn(input_size + hidden_size,
-                        hidden_size)  # forget gate weights
-        self.Wi = randn(input_size + hidden_size,
-                        hidden_size)  # input gate weights
-        self.Wc = randn(input_size + hidden_size,
-                        hidden_size)  # keep gate weights
-        self.Wo = rand(input_size + hidden_size,
-                       hidden_size)  # output gate weights
+        self.W_f = np.randn(input_size + hidden_size,
+                        hidden_size) / 1000  # forget gate weights
+        self.W_i = np.randn(input_size + hidden_size,
+                        hidden_size) / 1000  # input gate weights
+        self.W_c = np.randn(input_size + hidden_size,
+                        hidden_size) / 1000  # keep gate weights
+        self.W_o = np.rand(input_size + hidden_size,
+                       hidden_size) / 1000  # output gate weights
 
-        self.Wy = rand(input_size + hidden_size, hidden_size)
+        self.W_y = np.rand(input_size + hidden_size, hidden_size) / 1000
 
-        self.bf = np.zeros((hidden_size, 1))  # forget gate bias
-        self.bi = np.zeros((hidden_size, 1))  # input gate bias
-        self.bc = np.zeros((hidden_size, 1))  # candidate gate bias
-        self.bo = np.zeros((hidden_size, 1))  # output gate
-        self.by = np.zeros((hidden_size, 1))  # y bias
+        self.b_f = np.zeros((hidden_size, 1))  # forget gate bias
+        self.b_i = np.zeros((hidden_size, 1))  # input gate bias
+        self.b_c = np.zeros((hidden_size, 1))  # candidate gate bias
+        self.b_o = np.zeros((hidden_size, 1))  # output gate
+        self.b_y = np.zeros((hidden_size, 1))  # y bias
 
     def forward(self, inputs):
         # init first hidden state
-        h = np.zeros((self.Wf.shape[0], 1))
+        h = np.zeros((self.W_f.shape[0], 1))
         # store inputs for reference
         self.last_inputs = inputs
         # history of hiddent states
@@ -36,20 +36,27 @@ class LSTM(Layer):
             # concatenate inputs and last hidden state
             z = np.hstack((h, x))
             # forget gate
-            f_t = sigmoid(self.Wf @ z + bf)
-            i_t = sigmoid(self.Wi @ z + bi)
-            c_t = np.tanh(self.Wc @ z + bc)
+            f_t = sigmoid(self.W_f @ z + self.b_f)
+            i_t = sigmoid(self.W_i @ z + self.b_i)
+            c_t = np.tanh(self.W_c @ z + self.b_c)
 
-            self.Ct = f_t * self.Ct + i_t * c_t
+            self.C_t = f_t * self.C_t + i_t * c_t
 
-            o_t = sigmoid(self.Wo @ z + bo)
+            o_t = sigmoid(self.W_o @ z + self.b_o)
+            
+            h = o_t * np.tanh(self.C_t) # calculate new hidden state
+            
+            self.last_hs[i + 1] = h # save the hidden state
 
-            h = o_t * np.tanh(self.Ct)
 
-        # output vector
-        y = h @ self.Wy + by
+        # logits
+        y = h @ self.W_y + self.b_y
 
         return y, h
 
-    def backward(self):
-        pass
+    def backward(self, y_hat, y,  learn_rate=2e-2):
+        
+        
+
+
+
